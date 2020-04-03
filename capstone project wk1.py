d248e77f9827208a93f936cc41a651a6f97e84b7
nhuang5 <63116097+nhuang5@users.noreply.github.com>
2020-04-03 12:57:43 -0400

    Capstone Prj Wk1

---
+++ capstone project wk1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **This page is used for capstone project for IBM data science cert**
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
